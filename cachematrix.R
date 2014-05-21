d15c7afb5c509f5b3c9591ae98084fd3d4cdb006
naflint <flint.nicholas@gmail.com>
2014-05-21 17:21:04 +0200

    ready for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-## test github upload
-## Write a short comment describing this function
+## makeCacheMatrix and cacheSolve calculate the inverse of a matrix, and cache
+## the value so that it can be re-used if the calculation is repeated
+
+## makeCacheMatrix: takes a matrix and returns a list containing a four functions to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse of the matrix
+## get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(Inverse) m <<- Inverse
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: takes the output of makeCacheMatrix and returns the inverse
+## of the matrix given to makeCacheMatrix. However, it checks if it was previously 
+## cached by makeCacheMatrix, in which case it skips the calculation and takes the 
+## cached value. Otherwise it calculates the inverse of the matrix and sets the
+## value in the cache using the setInverse function of makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
